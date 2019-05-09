a4b58608433ec8101c19eebd805f75d7c41ccbe2
Elvin Mucaj <emuco7@yahoo.com>
2019-05-09 13:27:23 +0200

    Word Frequency from a file
    Word Frequency from a file

--- wordFrequency.py
+++ wordFrequency.py
@@ -1,25 +1,38 @@
 import sys
 
 def countWord(word, sentence):
-  count = 0
-  for i in range(len(sentence)):
-    if sentence[i] == word:
-      count += 1
-  return count
+	count = 0
+	for i in range(len(sentence)):
+		if sentence[i] == word:
+			count += 1
+	return count
 
 def countedWords(word, sentence, n):
-  found = False
-  for i in range(n):
-    if sentence[i] == word:
-      found = True
-  return found
+	found = False
+	for i in range(n):
+		if sentence[i] == word:
+			found = True
+	return found
+  
+def fileToList(fn):
+	txtfile = open(fn, "r")
+	txttable = []
+	for t in txtfile:
+		txttable.append(t.rstrip())
+	txtfile.close()
+	return txttable
   
 def wordFrequency(sentence):
-  sentence = sentence.split();
-  for i in range(len(sentence)):
-    if (countedWords(sentence[i], sentence, i) == True):
-      continue
-    else:
-      print sentence[i], '=', countWord(sentence[i], sentence)
-      
-wordFrequency('this mesage is the good message for the word frequecy test')
+	for i in range(len(sentence)):
+		if (countedWords(sentence[i], sentence, i) == True):
+			continue
+		else:
+			c = countWord(sentence[i], sentence)
+			if c > 1:
+				print sentence[i] + '=%s' % countWord(sentence[i], sentence)
+			
+if len(sys.argv) < 2:
+	print "[+] Usage: python",sys.argv[0]," <file.txt>;"
+else:
+	wordFrequency(fileToList(sys.argv[1]))
+
